bd7a3fdaeb11a1b3e92f1aa670e1ba46f25c93a5
RosieOh <dhxogns920@gmail.com>
2025-01-28 20:00:02 +0900

    FEAT : 문의하기 정보 추가

---
+++ src/components/contact/ContactInfo.tsx
@@ -0,0 +1,11 @@
+const ContactInfo = () => (
+    <div>
+      <h2>연락처</h2>
+      <p>전화: 123-456-7890</p>
+      <p>이메일: support@example.com</p>
+      <p>주소: 서울시 강남구 테헤란로 123</p>
+    </div>
+  );
+  
+  export default ContactInfo;
+  
