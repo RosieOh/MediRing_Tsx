145fd85e436b5f222816192903074b5595733064
RosieOh <dhxogns920@gmail.com>
2025-01-28 19:59:04 +0900

    FEAT : 백엔드 API 호출 Axios 함수 설정

---
+++ src/api/axios.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_URL = 'https://api.example.com/products';
+
+export const fetchProducts = async (category: string) => {
+  try {
+    const response = await axios.get(`${API_URL}?category=${category}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching products', error);
+    throw error;
+  }
+};
+
+export { };
