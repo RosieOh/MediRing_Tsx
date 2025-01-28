e397cb036e2e363c1996b36d8d984fe80cf8181a
RosieOh <dhxogns920@gmail.com>
2025-01-28 20:01:52 +0900

    FEAT : 로그인 폼 컴포넌트 활용으로 인한 페이지 분리

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "../../components/login/LoginForm";
+
+
+const Login = () => {
+return (
+        <div>
+            <LoginForm/>
+        </div>
+    );
+};
+
+export default Login;
