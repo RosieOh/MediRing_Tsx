3ad58ec4d1f14ecaa0ae87fad1c5465b914b1a88
RosieOh <dhxogns920@gmail.com>
2025-03-23 15:02:34 +0900

    CHORE : Footer 컴포넌트 디자인 수정

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,3 +1,4 @@
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
 import * as S from './style';
 
 const Footer = () => {
@@ -6,7 +7,7 @@ const Footer = () => {
       <S.FooterContents>
         {/* 브랜드 섹션 */}
         <S.BrandSection>
-          <S.Logo>내게 맞는 건강기능식품 찾기</S.Logo>
+          <S.Logo>MEDIRING</S.Logo>
           <S.FooterSlogan>당신의 건강을 위한 최적의 선택</S.FooterSlogan>
         </S.BrandSection>
 
@@ -20,19 +21,19 @@ const Footer = () => {
         {/* 소셜 미디어 섹션 */}
         <S.SocialContainer>
           <S.SocialIcon href="#" aria-label="Facebook">
-            {/* <S.FaFacebook /> */}
+            {FaFacebook({})}
           </S.SocialIcon>
           <S.SocialIcon href="#" aria-label="Twitter">
-            {/* <S.FaTwitter /> */}
+            {FaTwitter({})}
           </S.SocialIcon>
           <S.SocialIcon href="#" aria-label="Instagram">
-            {/* <S.FaInstagram /> */}
+            {FaInstagram({})}
           </S.SocialIcon>
         </S.SocialContainer>
       </S.FooterContents>
 
       {/* 저작권 정보 */}
-      <S.Copyright>© 2023 건강기능식품 추천 플랫폼. All rights reserved.</S.Copyright>
+      <S.Copyright>© 2024 MEDIRING. All rights reserved.</S.Copyright>
     </S.FooterWrapper>
   );
 };
