202455b8a6d04cb046d307fd535b11aec477c437
RosieOh <dhxogns920@gmail.com>
2025-03-23 15:01:21 +0900

    FEAT : 소개 페이지 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css';
 import MainCarousel from './components/carousel/MainCarousel';
 import Footer from './components/layout/footer/Footer';
 import Header from './components/layout/header/Header';
 import Hero from './components/layout/hero/Hero';
+import AboutPage from './pages/about/About';
 import Login from './pages/login/Login'; // 로그인 페이지 추가
 import SupportPage from './pages/support/SupportPage'; // 고객지원 페이지
 
@@ -26,6 +28,7 @@ const App: React.FC = () => {
           }
         />
         <Route path="/login" element={<Login />} /> {/* 로그인 페이지 라우트 추가 */}
+        <Route path="/about" element={<AboutPage />} />
       </Routes>
 
       <Footer />
