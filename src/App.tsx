bac584c723d0b10d3c159f9d3f7c238bed66ccd5
RosieOh <dhxogns920@gmail.com>
2025-03-23 15:11:34 +0900

    CHORE : 전체 디자인 수정

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,34 @@ import MainCarousel from './components/carousel/MainCarousel';
 import Footer from './components/layout/footer/Footer';
 import Header from './components/layout/header/Header';
 import Hero from './components/layout/hero/Hero';
-import AboutPage from './pages/about/About';
-import Login from './pages/login/Login'; // 로그인 페이지 추가
-import SupportPage from './pages/support/SupportPage'; // 고객지원 페이지
+import About from './pages/about/About';
+import Login from './pages/login/Login';
+import MyPage from './pages/mypage/MyPage';
+import SignUp from './pages/signup/SignUp';
+import SupportPage from './pages/support/SupportPage';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Header />
-
-      {/* 조건부로 Hero와 MainCarousel 렌더링 */}
-      <Routes>
-        <Route path="/support" element={<SupportPage />} /> {/* 고객지원 페이지 */}
-        {/* /support가 아닐 경우에만 Hero와 MainCarousel 렌더링 */}
-        <Route
-          path="*"
-          element={
-            <>
-              <Hero />
-              <MainCarousel />
-            </>
-          }
-        />
-        <Route path="/login" element={<Login />} /> {/* 로그인 페이지 라우트 추가 */}
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
-
-      <Footer />
+      <div className="App">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={
+              <>
+                <Hero />
+                <MainCarousel />
+              </>
+            } />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/mypage" element={<MyPage />} />
+            <Route path="/support" element={<SupportPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   );
 };
